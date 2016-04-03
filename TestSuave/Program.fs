54375b8d50b97a4125329d4472b220671302311f
Vicenç García Altés <vincentredrum@gmail.com>
2016-04-03 22:47:58 +0100

    Simple server

---
+++ TestSuave/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open Suave
+
+[<EntryPoint>]
+let main argv = 
+    startWebServer defaultConfig (Successful.OK "Hello World!")
+    0 // return an integer exit code
