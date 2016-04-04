ddef55499b023992a4d7d1830ec1354ceaead24a
Vicenç García Altés <vincentredrum@gmail.com>
2016-04-04 21:22:27 +0100

    Added resource group

--- TestSuave/Program.fs
+++ TestSuave/Program.fs
@@ -1,8 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
-open Suave
+open Suave // always open suave
+open Suave.Web // for config
+open System.Net
+ 
+let serverConfig port = 
+    let port = Sockets.Port.Parse <| port
+    { defaultConfig with
+       bindings = [ HttpBinding.mk HTTP IPAddress.Loopback port ]
+    }
 
 [<EntryPoint>]
-let main [|port; staticFileLocation|] = 
-    startWebServer defaultConfig (Successful.OK "Hello World!")
+let main argsv = 
+    let port = argsv.[0]
+    startWebServer (serverConfig port) (Successful.OK "Hello World from Suave!")
     0 // return an integer exit code
